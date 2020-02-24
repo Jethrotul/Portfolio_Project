93ecbd72aa83095c65c28f57ee24bf6c50bfcf2a
Diego Fernandez <diego.fernandez.perez@outlook.es>
2020-02-24 19:50:09 +0100

    Acabando estructura de la web

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -6,10 +6,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
-
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
-  }
+    const navToggle = document.querySelector('.nav-toggle');
+    const navLinks = document.querySelectorAll('.nav__link');
 
+    navToggle.addEventListener('click', () => {
+      document.body.classList.toggle('nav-open');
+    });
+
+    navLinks.forEach(link => {
+      link.addEventListener('click', () => {
+        document.body.classList.remove('nav-open');
+      });
+    });
+  }
 }
